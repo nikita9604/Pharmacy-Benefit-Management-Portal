b73b7b8efb099cc7d9e7ea828a204667eecbd596
SHRESHTHA SINGH <47150964+shreshtha99@users.noreply.github.com>
2021-08-04 20:48:59 +0530

    Updated Drug Repository

--- spring-webflux-project-master/src/main/java/com/springwebfluxproject/springwebfluxproject/repository/DrugRepository.java
+++ spring-webflux-project-master/src/main/java/com/springwebfluxproject/springwebfluxproject/repository/DrugRepository.java
@@ -6,4 +6,6 @@ import com.springwebfluxproject.springwebfluxproject.entity.Drug;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 public interface DrugRepository extends ReactiveCrudRepository<Drug,Integer> {
+  @Query("select count(*) > 0 from Drug where dname = :dname")
+    Mono<Boolean> hasDrug(String dname);
 }
