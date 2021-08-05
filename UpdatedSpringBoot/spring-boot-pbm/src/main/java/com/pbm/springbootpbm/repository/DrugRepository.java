51047cf6df5b78822d07094683f825b22f53724a
Nikita Rath <54584131+nikita9604@users.noreply.github.com>
2021-08-05 14:53:10 +0530

    Delete UpdatedSpringBoot/spring-boot-pbm directory

--- UpdatedSpringBoot/spring-boot-pbm/src/main/java/com/pbm/springbootpbm/repository/DrugRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pbm.springbootpbm.repository;
-
-import com.pbm.springbootpbm.entity.Drug;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DrugRepository extends JpaRepository<Drug,Integer> {
-}
