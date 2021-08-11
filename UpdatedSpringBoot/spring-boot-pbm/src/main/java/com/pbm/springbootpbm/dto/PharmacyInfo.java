b56c4c27f6f9cccab15341e8208c9864f3d02b84
Akshara Pramod <67370584+akshara1610@users.noreply.github.com>
2021-08-11 23:09:40 +0530

    Delete UpdatedSpringBoot directory

--- UpdatedSpringBoot/spring-boot-pbm/src/main/java/com/pbm/springbootpbm/dto/PharmacyInfo.java
+++
@@ -1,9 +0,0 @@
-package com.pbm.springbootpbm.dto;
-
-import lombok.Value;
-
-@Value
-public class PharmacyInfo {
-    // Pharmacy Name, Pharmacy City (Pharmacy) & Drug Name (Drug) join by (Record)
-    String phname, city, dname;
-}
--- UpdatedSpringBoot/spring-boot-pbm/src/main/java/com/pbm/springbootpbm/dto/RecordWithPharmacy.java
+++
@@ -1,9 +0,0 @@
-package com.pbm.springbootpbm.dto;
-
-import com.pbm.springbootpbm.entity.Pharmacy;
-import com.pbm.springbootpbm.entity.Record;
-
-public interface RecordWithPharmacy {
-    public Record getRecord();
-    public Pharmacy getPharmacy();
-}
